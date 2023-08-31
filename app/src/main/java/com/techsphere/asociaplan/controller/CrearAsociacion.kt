dfbfd3424322ca90e050fb0521a62f54762e1943
Manuel Rodríguez <m.alejandro00@estudiantec.cr>
2023-08-31 00:19:57 -0600

    Intento funciones registrar asociacion (aún no esta el sp)

---
+++ app/src/main/java/com/techsphere/asociaplan/controller/CrearAsociacion.kt
@@ -0,0 +1,39 @@
+package com.techsphere.asociaplan.controller
+
+import android.util.Log
+import java.sql.*
+
+private const val connectionString : String = "jdbc:jtds:sqlserver://serverapp-ap.database.windows.net;databaseName=BDAppAP;"+
+        "user=adminEvenAP;password=EventosAP1;ssl=require"
+
+suspend fun registerAsocInBD(Nombre : String, Contacto : String, Codigo : String, Descripcion: String
+) : Int{
+    var conn : Connection? = null
+    try {
+        Class.forName("net.sourceforge.jtds.jdbc.Driver")
+        conn = DriverManager.getConnection(connectionString)
+        var cs = conn.prepareCall("{call ModificarTiempoUso @inCodigo=?, @inEstado=?, @inCantidadTiempo=?, @inFechaInicio=?, " +
+                "@inFechaFin=?, @outCodeResult=?}")
+        // Asumimos que se nos pasan valores no nulos
+        cs.setString(1, Nombre)
+        cs.setString(2, Contacto)
+        cs.setString(3, Codigo)
+        cs.setString(4, Descripcion)
+        // Le indicamos el parametro de salida y su tipo
+        cs.registerOutParameter(6, Types.INTEGER)
+        // Se ejecuta la query
+        cs.execute()
+        var result = cs.getInt(6)
+        Log.i("SP OutCode", "Resultado: "+result.toString())
+        return result
+    }catch (ex: SQLException){
+        Log.e("Error SQL Exception: ", ex.message.toString())
+        return 0
+    }catch (ex1: ClassNotFoundException){
+        Log.e("Error Class Not Found: ", ex1.message.toString())
+        return 0
+    }catch (ex2: Exception) {
+        Log.e("Error Exception: ", ex2.message.toString())
+        return 0
+    }
+}
--- app/src/main/java/com/techsphere/asociaplan/view/registrar_asociacion.kt
+++ app/src/main/java/com/techsphere/asociaplan/view/registrar_asociacion.kt
@@ -1,11 +1,81 @@
 package com.techsphere.asociaplan.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.techsphere.asociaplan.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.techsphere.asociaplan.controller.registerAsocInBD
+import java.sql.Date
+import java.util.*
 
 class registrar_asociacion : AppCompatActivity() {
+    private lateinit var txtNombre : EditText
+    private lateinit var txtContacto : EditText
+    private lateinit var txtCod : EditText
+    private lateinit var txtDescripcion : EditText
+    private lateinit var btnRegis : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_asociacion)
+        txtNombre = findViewById(R.id.nombre_asociacion)
+        txtContacto = findViewById(R.id.contacto)
+        txtCod = findViewById(R.id.codigo_carrera)
+        txtDescripcion = findViewById(R.id.descripcion)
+        btnRegis = findViewById(R.id.button_registrar)
+        btnRegis.setOnClickListener {
+            Registrar()
+        }
+    }
+
+    fun Registrar() {
+        val Nombre = txtNombre.text.toString()
+        val Contacto = txtContacto.text.toString()
+        val Codigo = txtCod.text.toString()
+        val Descripcion = txtDescripcion.text.toString()
+        txtCod.error=null
+        txtDescripcion.error=null
+        txtContacto.error=null
+        txtNombre.error=null
+        if (Nombre.isEmpty()||Nombre.isBlank()){
+            txtNombre.error="Por favor introduzca el nombre"
+            return
+        }
+        if (Contacto.isEmpty()||Contacto.isBlank()){
+            txtContacto.error="Por favor introduzca el contacto"
+            return
+        }
+        if (Codigo.isEmpty()||Codigo.isBlank()){
+            txtCod.error="Por favor introduzca el Codigo"
+            return
+        }
+        if (Descripcion.isEmpty()||Descripcion.isBlank()){
+            txtDescripcion.error="Por favor introduzca la descripcion"
+            return
+        }
+
+        Toast.makeText(this, "Registrando la asociación", Toast.LENGTH_SHORT).show()
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = registerAsocInBD(Nombre, Contacto,
+                Codigo, Descripcion
+            )
+            if (res == 1){
+                //startActivity(Intent(this@registrar_asociacion,"Inserte menu"::class.java))
+                finish()
+            } else {
+                // Se le muestra el dialogo al usuario
+                withContext(Dispatchers.Main){
+                    // El withContext se usa para llamar funciones que solo se pueden llamar
+                    Toast.makeText(this@registrar_asociacion,"No se registro la asociación.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
