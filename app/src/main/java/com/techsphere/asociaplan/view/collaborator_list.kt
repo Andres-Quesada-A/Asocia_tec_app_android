bde5bb0f32e309ff9a0a38fb935ee6c7b27ef80c
Andres-QA <a.quesada@newstreetdev.com>
2023-09-05 06:15:38 -0600

    collaborator_list adn rv_collaborator

---
+++ app/src/main/java/com/techsphere/asociaplan/view/collaborator_list.kt
@@ -0,0 +1,11 @@
+package com.techsphere.asociaplan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class collaborator_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collaborator_list)
+    }
+}
