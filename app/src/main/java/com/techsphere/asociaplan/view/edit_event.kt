0583170f7b4d057ed191055fb729363691b562bc
Andres-QA <a.quesada@newstreetdev.com>
2023-09-05 12:02:40 -0600

    edit event and details

---
+++ app/src/main/java/com/techsphere/asociaplan/view/edit_event.kt
@@ -0,0 +1,11 @@
+package com.techsphere.asociaplan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edit_event : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_event)
+    }
+}
---
+++ app/src/main/java/com/techsphere/asociaplan/view/event_details.kt
@@ -0,0 +1,11 @@
+package com.techsphere.asociaplan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class event_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_event)
+    }
+}
