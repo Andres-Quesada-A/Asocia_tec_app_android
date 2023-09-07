84c403c52241f5621e5e0259b0f5105fbd3d856c
Andres-QA <a.quesada@newstreetdev.com>
2023-09-06 18:03:17 -0600

    add_to_event, search collaborator and rv list of collaborator

---
+++ app/src/main/java/com/techsphere/asociaplan/view/add_to_event_activity.kt
@@ -0,0 +1,11 @@
+package com.techsphere.asociaplan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_to_event_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_to_event)
+    }
+}
