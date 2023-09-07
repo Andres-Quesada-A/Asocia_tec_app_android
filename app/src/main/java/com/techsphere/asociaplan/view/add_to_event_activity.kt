2a30af5a1796bfc3e0d7983a1576eaa40c1dead2
Manuel Rodríguez <m.alejandro00@estudiantec.cr>
2023-09-07 12:41:19 -0600

    Lo que borre

---
+++ app/src/main/java/com/techsphere/asociaplan/view/add_to_event_activity.kt
@@ -0,0 +1,12 @@
+package com.techsphere.asociaplan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.techsphere.asociaplan.R
+
+class add_to_event_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_to_event)
+    }
+}
